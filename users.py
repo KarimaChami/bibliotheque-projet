adcdbe1f4a1e0f1e9ef044efb16a79c618d7f103
KarimaChami <karimachami24@gmail.com>
2025-09-26 12:04:55 +0100

    add new code

--- users.py
+++ users.py
@@ -1,4 +1,9 @@
 from data import *
-
+#Qstion 1
 utilisateurs_majeurs = list(filter(lambda user : user[3] >= 18, utilisateurs))
 print(utilisateurs_majeurs)
+#Qstion 2
+noms_majuscule = list(map(lambda user : user[1].upper(), utilisateurs))
+print(noms_majuscule)
+#Qstion 3
+
