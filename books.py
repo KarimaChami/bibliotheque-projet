307beb49fe19b5cca5f8c93eae01a62fa8deb791
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-09-26 16:56:13 +0100

    new update

--- books.py
+++ books.py
@@ -5,8 +5,6 @@ livres_trier = sorted(livres, key=lambda x: x["année"])
 
 print(f"le livre le plus ancien est : {livres_trier[0]["titre"]} et le livre le plus resent est : {livres_trier[2]["titre"]} ")
 
-# dictionnaire = dict(aime_livres)
-# print(dictionnaire)
 List = []
 for tup in aime_livres:
     List.append(tup[1])
@@ -22,10 +20,4 @@ for L in List:
 
 
 
-    """ titre_livre = tup[1]
-    print(titre)
-    user_id = 0
-    if titre_livre == titre_livre:
-      user_id += 1
-      txt = f"{titre_livre }:{user_id}"
-    List.append(txt) """
+   
